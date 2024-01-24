99f65c37b9ee3bb6950827f9046e0a44ddd3953e
Bimendra Bandara <bimendra.85@gmail.com>
2024-01-24 13:32:54 +0000

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Button } from "../@/components/ui/button";
+
 export function App() {
-  return <h1>Hello</h1>
+  return (
+    <div>
+      <h1 className="font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight">
+        Hello
+      </h1>
+    </div>
+  );
 }
